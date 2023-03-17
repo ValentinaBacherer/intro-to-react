2bacfed8bbf4722187bca51873ba884b57c20a1c
Valentina Bacherer <valentina.bacherer@gmail.com>
2023-03-17 09:16:46 -0300

    add components with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css';
 import SubTitle from './components/SubTitle';
 import Title from './components/Title';
+import { Text } from './components/Text';
+
+const libros = ['El principito', 'El mundo de sofia', 'Hamlet'];
 
 function App() {
   return (
     <div className='App'>
-      <Title />
+      {libros.map((libro) => {
+        return <Title frase={libro} color='violet' />;
+      })}
       <SubTitle />
+      <Text />
     </div>
   );
 }
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,4 +1,10 @@
-function Title() {
-  return <h1>Esto es un title</h1>;
+import { Text } from './Text';
+function Title(props) {
+  return (
+    <div>
+      <h1 style={{ backgroundColor: `${props.color}` }}>{props.frase}</h1>
+      <Text />
+    </div>
+  );
 }
 export default Title;
